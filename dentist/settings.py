7e7035ea3c1717bc3d452e9a3999e5be47d827b0
Umar Zakari <umarzaks@yahoo.com>
2020-05-20 12:21:04 +0100

    edited settings.py

--- dentist/settings.py
+++ dentist/settings.py
@@ -128,8 +128,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 # EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'temail659@gmail.com'
-EMAIL_HOST_PASSWORD = '!9g.5A<s~Jcj}zDx'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_SSL = True
 # EMAIL_USE_TLS = True
 
